bf5d43a6404cf5832266f396aa9174a69409e7bc
Satyasiddharth7 <satyapatra628@gmail.com>
2026-02-26 00:25:30 +0530

    Added server setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server running successfully ✅");
+});
+
+app.listen(5000, () => {
+  console.log("Server started on http://localhost:5000");
+});
